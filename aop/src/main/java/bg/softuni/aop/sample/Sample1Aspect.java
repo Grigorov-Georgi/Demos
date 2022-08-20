f138fedb322439f6ad8e87832362353d2eb8a59f
Georgi Grigorov <grigorov_georgi@yahoo.com>
2022-08-20 11:17:38 +0300

    Added AfterThrowing advice

--- aop/src/main/java/bg/softuni/aop/sample/Sample1Aspect.java
+++ aop/src/main/java/bg/softuni/aop/sample/Sample1Aspect.java
@@ -1,6 +1,7 @@
 package bg.softuni.aop.sample;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -27,5 +28,14 @@ public class Sample1Aspect {
         LOGGER.info("Before calling method {} with arguments {}",
                 jp.getSignature(),
                 Arrays.asList(jp.getArgs()));
+    }
+
+    @AfterThrowing
+            (pointcut = "execution(* bg.softuni.aop.IncredibleMachine.boom(..))",
+            throwing = "error")
+    public void afterThrowing(JoinPoint jp, Throwable error){
+        LOGGER.error("Ups, I think that the method {} threw Exception and exception is:",
+                jp.getSignature(),
+                error);
     }
 }
